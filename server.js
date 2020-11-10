49f6f2cd9b0a6bcb0c1944a707ae980bc943da31
arnavozil <arnavpanwar99@gmail.com>
2020-11-10 20:25:43 +0530

    added cors

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+app.use(cors());
 app.use(bodyParser.json());
 
 
